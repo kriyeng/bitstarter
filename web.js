146c2236de99189c79c2202c666371bb675278b1
David Ibanyez <kriyeng@gmail.com>
2013-07-13 22:04:57 +0000

    Testing web.js

--- web.js
+++ web.js
@@ -3,10 +3,22 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readFile());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+function readFile(){
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+
+    var buf=data;
+    return buf.toString();
+});
+
+}
